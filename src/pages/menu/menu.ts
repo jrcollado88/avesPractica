eeb8812348f11a90dbf6cf7606926ce40664e622
Juanra Collado <juanra_perello@hotmail.com>
2018-05-14 20:40:18 +0200

    Menu finalizado

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
-/**
- * Generated class for the MenuPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
+
+
 export class MenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public toastCtrl: ToastController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuPage');
+
+  not_ready(){
+    this.toastCtrl.create({
+      message: "Esta funcionalidad no está disponible en esta PEC.",
+      duration: 3000,
+      position: 'top'
+    }).present();
   }
 
+  logout(){
+    this.navCtrl.pop();
+  }
 }
